5fc108d2dc1548d0882dd4a8a5610ec7a72b4426
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2024-11-03 22:26:37 +0900

    Update index.js
    store/index.js 랑 router/index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#store/index.js
 import { createStore } from 'vuex';
 import axios from 'axios';
 
@@ -48,4 +49,43 @@ export default createStore({
   // modules: 상태를 모듈별로 나누어 관리할 때 사용합니다 (현재는 빈 객체로 설정).
   modules: {}
 })
+
+#router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AccountRegister from '@/views/AccountRegister.vue'
+import AccountLogin from '@/views/AccountLogin.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/account',
+    name: 'account',
+    component: AccountRegister
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: AccountLogin
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
+
 
