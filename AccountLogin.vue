cbb388862afec716414745708a4f343575730d9e
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2024-11-06 01:57:18 +0900

    Update AccountLogin.vue
    로그인 성공시 mainPage로 이동하게 설정

--- AccountLogin.vue
+++ AccountLogin.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container style="width: 450px" class = "login">
+    <v-container style="width: 450px" class="login">
       <v-layout align-center row wrap>
         <v-flex xs12>
           <v-alert v-if="isError" type="error">
@@ -7,9 +7,9 @@
           </v-alert>
           <v-card>
             <v-toolbar flat color="indigo">
-              <v-toolbar-title
-                ><span class="white--text">로그인</span></v-toolbar-title
-              >
+              <v-toolbar-title>
+                <span class="white--text">로그인</span>
+              </v-toolbar-title>
             </v-toolbar>
             <div class="pa-5">
               <v-form ref="form" v-model="valid" lazy-validation>
@@ -19,7 +19,7 @@
                   label="Enter E-mail"
                   required
                 ></v-text-field>
-  
+
                 <v-text-field
                   v-model="formData.password"
                   :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
@@ -30,10 +30,10 @@
                   counter
                   @click:append="show = !show"
                 ></v-text-field>
-  
+
                 <div class="mt-3 d-flex flex-row-reverse">
                   <v-btn color="error" class="mr-4" @click="reset"> 리셋 </v-btn>
-  
+
                   <v-btn
                     color="primary"
                     class="mr-4"
@@ -43,7 +43,7 @@
                   >
                     회원가입
                   </v-btn>
-  
+
                   <v-btn
                     :disabled="!valid"
                     color="success"
@@ -59,58 +59,62 @@
         </v-flex>
       </v-layout>
     </v-container>
-  </template>
-  
-  <script>
-  import LoginObj from "../store/loginObj"
-  import axios from "axios"
-  export default {
-    data: () => ({
-      formData: new LoginObj("", ""),
-      valid: false,
-      isError: false,
-      errorMsg: "",
-      emailRules: [
-        (v) => !!v || "E-mail is required",
-        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid"
-      ],
-      show: false,
-      rules: {
-        required: (value) => !!value || "Required.",
-        min: (v) => v.length >= 8 || "Min 8 characters"
+</template>
+
+<script>
+import LoginObj from "../store/loginObj";
+import axios from "axios";
+
+export default {
+  data: () => ({
+    formData: new LoginObj("", ""),
+    valid: false,
+    isError: false,
+    errorMsg: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    show: false,
+    rules: {
+      required: (value) => !!value || "Required.",
+      min: (v) => v.length >= 8 || "Min 8 characters"
+    }
+  }),
+  methods: {
+    login(LoginObj) {
+      // 이메일과 비밀번호가 입력되었는지 체크
+      if (!this.formData.email || !this.formData.password) {
+        this.isError = true;
+        this.errorMsg = "이메일과 비밀번호를 입력해주세요.";
+        return;
       }
-    }),
-    methods: {
-      login(LoginObj) {
-        if (!this.formData.email || !this.formData.password) {
-          this.isError = true
-          this.errorMsg = "이메일과 비밀번호를 입력해주세요."
-          return
-        }
-        axios
-          .post("/signin", LoginObj)
-          .then((res) => {
-            let token = res.data.token
-            localStorage.setItem("access_token", token)
-            this.$store.dispatch("getAccountInfo")
-            this.$router.push({ name: "Home" })
-          })
-          .catch((err) => {
-            if (err.response) {
-              this.isError = true
-              this.errorMsg = err.response.data.message
-            }
-          })
-      },
-      validate() {
-        this.$refs.form.validate()
-      },
-      reset() {
-        this.$refs.form.reset()
-      },
-      resetValidation() {
-        this.$refs.form.resetValidation()
-      }
+      // Axios를 사용하여 서버에 로그인 요청
+      axios
+        .post("/signin", LoginObj)
+        .then((res) => {
+          // 로그인 성공 시 토큰을 로컬 스토리지에 저장
+          let token = res.data.token;
+          localStorage.setItem("access_token", token);
+          this.$store.dispatch("getAccountInfo"); // 사용자 정보를 가져오는 스토어 액션 호출
+          this.$router.push({ name: "MainPage" }); // 메인 페이지로 이동
+        })
+        .catch((err) => {
+          if (err.response) {
+            this.isError = true;
+            this.errorMsg = err.response.data.message; // 에러 메시지 설정
+          }
+        });
+    },
+    validate() {
+      this.$refs.form.validate(); // 폼 검증 수행
+    },
+    reset() {
+      this.$refs.form.reset(); // 폼 초기화
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation(); // 유효성 초기화
     }
   }
-  </script>
+}
+</script>
