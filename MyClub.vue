9df346ffc90a9e1a3ef004fad13dfa4a42df9677
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2024-11-14 00:15:30 +0900

    승인 요청 버튼을 만들고 승인 여부 확인 팝업 구현

--- MyClub.vue
+++ MyClub.vue
@@ -1,16 +1,34 @@
 <template>
-    <v-container class="MyClub">
-      <h1>hello</h1>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    name: "myClub",
-  };
-  </script>
-  
-  <style scoped>
-  /* 스타일을 여기에 추가하세요 */
-  </style>
-  
+  <v-container>
+    <h1>내 동아리</h1>
+    <v-row>
+      <v-col
+        v-for="club in clubs"
+        :key="club.id"
+        cols="12" sm="6" md="4"
+      >
+        <v-card @click="navigateToClub(club.id)" class="club-card">
+          <v-card-title>{{ club.name }}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clubs: [
+        { id: '1', name: '축구 동아리' },
+        { id: '2', name: '음악 동아리' }
+      ]
+    };
+  },
+  methods: {
+    navigateToClub(id) {
+      this.$router.push({ name: 'ClubDetail', params: { id } });
+    }
+  }
+};
+</script>
