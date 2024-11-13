e7ea4c3df615133e61099a4022f9d36fc4d6c3f1
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2024-11-14 00:42:58 +0900

    메뉴 바 추가 하였음

--- MyClub.vue
+++ MyClub.vue
@@ -1,6 +1,21 @@
 <template>
   <v-container>
-    <h1>내 동아리</h1>
+    <!-- 제목과 메뉴 아이콘 -->
+    <v-row align="center" justify="space-between">
+      <v-col cols="2"></v-col> <!-- 빈 공간을 추가하여 텍스트를 중앙으로 -->
+      <v-col class="text-center">
+        <h1>내 동아리</h1>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn icon color="white" @click="drawer = true" title="메뉴">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+
+
+    <!-- 동아리 목록 -->
     <v-row>
       <v-col
         v-for="club in clubs"
@@ -12,6 +27,21 @@
         </v-card>
       </v-col>
     </v-row>
+
+    <!-- 오른쪽에서 나오는 슬라이드 네비게이션 메뉴 -->
+    <v-navigation-drawer v-model="drawer" app temporary location = 'right' dark color="rgba(0, 0, 0, 0.8)">
+      <v-list>
+        <v-list-item @click="navigateTo('mainPage')">
+          <v-list-item-title class="white-text">메인 페이지</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="navigateTo('myClub')">
+          <v-list-item-title class="white-text">내 동아리</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="navigateTo('clubSearch')">
+          <v-list-item-title class="white-text">동아리 검색</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </v-container>
 </template>
 
@@ -19,6 +49,7 @@
 export default {
   data() {
     return {
+      drawer: false, // 슬라이드 메뉴 상태
       clubs: [
         { id: '1', name: '축구 동아리' },
         { id: '2', name: '음악 동아리' }
@@ -28,7 +59,21 @@ export default {
   methods: {
     navigateToClub(id) {
       this.$router.push({ name: 'ClubDetail', params: { id } });
+    },
+    navigateTo(routeName) {
+      this.drawer = false; // 메뉴 닫기
+      this.$router.push({ name: routeName });
     }
   }
 };
 </script>
+
+<style scoped>
+.v-navigation-drawer__content {
+  background-color: rgba(0, 0, 0, 0.8) !important;
+}
+
+.white-text {
+  color: white !important;
+}
+</style>
