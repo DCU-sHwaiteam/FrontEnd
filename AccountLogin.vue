666346eff395fe265e0be88e3af5b7ec493d5baa
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2025-03-03 22:42:58 +0900

    로그인 백엔드 연동

--- AccountLogin.vue
+++ AccountLogin.vue
@@ -1,123 +1,106 @@
 <template>
-    <v-container style="width: 450px" class="login">
-      <v-layout align-center row wrap>
-        <v-flex xs12>
-          <v-alert v-if="isError" type="error">
-            {{ errorMsg }}
-          </v-alert>
-          <v-card>
-            <v-toolbar flat color="indigo">
-              <v-toolbar-title>
-                <span class="white--text">로그인</span>
-              </v-toolbar-title>
-            </v-toolbar>
-            <div class="pa-5">
-              <v-form ref="form" v-model="valid" lazy-validation>
-                <v-text-field
-                  v-model="formData.email"
-                  :rules="emailRules"
-                  label="Enter E-mail"
-                  required
-                ></v-text-field>
+  <v-container style="width: 450px" class="login">
+    <v-layout align-center row wrap>
+      <v-flex xs12>
+        <v-alert v-if="isError" type="error">
+          {{ errorMsg }}
+        </v-alert>
+        <v-card>
+          <v-toolbar flat color="indigo">
+            <v-toolbar-title>
+              <span class="white--text">로그인</span>
+            </v-toolbar-title>
+          </v-toolbar>
+          <div class="pa-5">
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                v-model="inputUsername"
+                :rules="[rules.required]"
+                label="아이디"
+                required
+              ></v-text-field>
 
-                <v-text-field
-                  v-model="formData.password"
-                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                  :rules="[rules.required, rules.min]"
-                  :type="show ? 'text' : 'password'"
-                  label="Enter Password"
-                  hint="At least 8 characters"
-                  counter
-                  @click:append="show = !show"
-                ></v-text-field>
+              <v-text-field
+                v-model="inputPassword"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[rules.required, rules.min]"
+                :type="show ? 'text' : 'password'"
+                label="비밀번호"
+                hint="최소 8자"
+                counter
+                @click:append="show = !show"
+              ></v-text-field>
 
-                <div class="mt-3 d-flex flex-row-reverse">
-                  <v-btn color="error" class="mr-4" @click="reset"> 리셋 </v-btn>
+              <div class="mt-3 d-flex flex-row-reverse">
+                <v-btn color="error" class="mr-4" @click="reset">리셋</v-btn>
 
-                  <v-btn
-                    color="primary"
-                    class="mr-4"
-                    link
-                    router
-                    :to="{ name: 'account' }"
-                  >
-                    회원가입
-                  </v-btn>
+                <v-btn color="primary" class="mr-4" link router :to="{ name: 'account' }">
+                  회원가입
+                </v-btn>
 
-                  <v-btn
-                    :disabled="!valid"
-                    color="success"
-                    class="mr-4"
-                    @click="login(formData)"
-                  >
-                    로그인
-                  </v-btn>
-                </div>
-              </v-form>
-            </div>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+                <v-btn
+                  :disabled="!valid"
+                  color="success"
+                  class="mr-4"
+                  @click="login"
+                >
+                  로그인
+                </v-btn>
+              </div>
+            </v-form>
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import LoginObj from "../store/loginObj";
-import axios from "axios";
+import { loginUser } from "@/services/authService"; // 로그인 API 추가
 
 export default {
-  data: () => ({
-    formData: new LoginObj("", ""),
-    valid: false,
-    isError: false,
-    errorMsg: "",
-    emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid"
-    ],
-    show: false,
-    rules: {
-      required: (value) => !!value || "Required.",
-      min: (v) => v.length >= 8 || "Min 8 characters"
-    }
-  }),
+  data() {
+    return {
+      inputUsername: '',
+      inputPassword: '',
+      valid: false,
+      isError: false,
+      errorMsg: '',
+      show: false,
+      rules: {
+        required: (value) => !!value || '필수 항목입니다.',
+        min: (v) => v.length >= 8 || '최소 8자',
+      },
+    };
+  },
   methods: {
-    login(LoginObj) {
-      // 이메일과 비밀번호가 입력되었는지 체크
-      if (!this.formData.email || !this.formData.password) {
-        this.isError = true;
-        this.errorMsg = "이메일과 비밀번호를 입력해주세요.";
-        return;
-      }
-      // Axios를 사용하여 서버에 로그인 요청
-      axios
-        .post("/signin", LoginObj)
-        .then((res) => {
-          // 로그인 성공 시 토큰을 로컬 스토리지에 저장
-          let token = res.data.token;
-          localStorage.setItem("access_token", token);
-          this.$store.dispatch("getAccountInfo"); // 사용자 정보를 가져오는 스토어 액션 호출
-          this.$router.push({ name: "MainPage" }); // 메인 페이지로 이동
-        })
-        .catch((err) => {
-          if (err.response) {
-            this.isError = true;
-            this.errorMsg = err.response.data.message; // 에러 메시지 설정
-          }
+    async login() {
+      try {
+        const response = await loginUser({
+          username: this.inputUsername,  // 로그인 시 email을 username으로 사용
+          password: this.inputPassword
         });
-    },
-    validate() {
-      this.$refs.form.validate(); // 폼 검증 수행
+
+        // 로그인 성공 시 토큰 저장
+        localStorage.setItem("token", response.token);
+
+        // 메인 페이지로 이동
+        this.$router.push({ name: "mainPage" });
+      } catch (error) {
+        this.isError = true;
+        this.errorMsg = "아이디 또는 비밀번호가 잘못되었습니다.";
+      }
     },
     reset() {
-      this.$refs.form.reset(); // 폼 초기화
+      this.inputUsername = '';
+      this.inputPassword = '';
+      this.isError = false;
+      this.errorMsg = '';
     },
-    resetValidation() {
-      this.$refs.form.resetValidation(); // 유효성 초기화
-    }
-  }
-}
+  },
+};
 </script>
+
 
 ----------------------------------------
 #학생정보 백엔드 구축되기 전까지 임의로 아이디 비번 설정해서 로그인 되게 하였음
