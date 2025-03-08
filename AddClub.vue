4d3bc995a8e8d15e0404f30f5410b290b4c9a46b
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2025-03-08 18:13:05 +0900

    수정

--- AddClub.vue
+++ AddClub.vue
@@ -1,126 +1,120 @@
 <template>
-    <v-container>
-      <h1>동아리 추가 페이지</h1>
-  
-      <!-- 동아리 추가 양식 -->
-      <v-form ref="form" v-model="isFormValid">
-        <v-text-field
-          label="동아리 이름"
-          v-model="clubName"
-          required
-        ></v-text-field>
-  
-        <v-text-field
-          label="동아리 장 이름"
-          v-model="leaderName"
-          required
-        ></v-text-field>
-  
-        <v-text-field
-          label="동아리 장 학번"
-          v-model="leaderMail"
-          required
-        ></v-text-field>
+  <v-container>
+    <h1>동아리 추가 페이지</h1>
 
-        <v-text-field
-          label="동아리 장 아이디"
-          v-model="leaderId"
-          required
-        ></v-text-field>
-  
-        <v-text-field
-          label="지도 교수"
-          v-model="advisor"
-          required
-        ></v-text-field>
-  
-        <v-text-field
-          label="최대 인원수"
-          v-model="maxMembers"
-          type="number"
-          required
-        ></v-text-field>
-  
-        <v-text-field
-          label="동아리 활동 날짜와 시간"
-          v-model="activitySchedule"
-          hint="예: 매주 화요일 5시"
-          required
-        ></v-text-field>
-  
-        <v-text-field
-          label="태그 (콤마로 구분)"
-          v-model="tags"
-        ></v-text-field>
-  
-        <v-textarea
-          label="한 줄 소개"
-          v-model="description"
-          rows="2"
-          maxlength="100"
-          hint="최대 100자까지 입력 가능합니다."
-        ></v-textarea>
-  
-        <!-- 버튼 -->
-        <v-row class="button-row" justify="space-between" align="center">
-          <v-btn @click="navigateToSearch" color="grey" outlined>돌아가기</v-btn>
-          <v-btn @click="registerClub" color="primary" :disabled="!isFormValid">등록</v-btn>
-        </v-row>
-      </v-form>
-    </v-container>
-  </template>
-  
-  <script>
+    <!-- 동아리 추가 양식 -->
+    <v-form ref="form" v-model="isFormValid">
+      <v-text-field
+        label="동아리 이름"
+        v-model="clubName"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        label="동아리 장 이름"
+        v-model="leaderName"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        label="동아리 장 이메일"
+        v-model="leaderEmail"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        label="지도 교수"
+        v-model="advisor"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        label="최대 인원수"
+        v-model="maxMembers"
+        type="number"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        label="동아리 활동 날짜와 시간"
+        v-model="activitySchedule"
+        hint="예: 매주 화요일 5시"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        label="태그 (콤마로 구분)"
+        v-model="tags"
+      ></v-text-field>
+
+      <v-textarea
+        label="한 줄 소개"
+        v-model="description"
+        rows="2"
+        maxlength="100"
+        hint="최대 100자까지 입력 가능합니다."
+      ></v-textarea>
+
+      <!-- 버튼 -->
+      <v-row class="button-row" justify="space-between" align="center">
+        <v-btn @click="navigateToSearch" color="grey" outlined>돌아가기</v-btn>
+        <v-btn @click="registerClub" color="primary" :disabled="!isFormValid">등록</v-btn>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import { createClub } from "@/services/authService";
+
 export default {
-  data() {
-    return {
-      clubName: '',
-      leaderName: '',
-      leaderId: '',
-      advisor: '',
-      maxMembers: '',
-      activitySchedule: '',
-      tags: '',
-      description: '',
-      isFormValid: false,
-    };
-  },
-  methods: {
-    navigateToSearch() {
-      this.$router.push({ name: 'clubSearch' });
-    },
-    registerClub() {
-      if (this.isFormValid) {
-        const newClub = {
-          id: Date.now().toString(), // ✅ 동아리 ID를 현재 시간으로 생성 (유니크한 값)
-          name: this.clubName,
-          leader: this.leaderName,
-          leaderId: this.leaderId,
-          advisor: this.advisor,
-          maxMembers: this.maxMembers,
-          currentMembers: 0,
-          activitySchedule: this.activitySchedule,
-          tags: this.tags,
-          description: this.description
-        };
-
-        let clubs = JSON.parse(localStorage.getItem('clubs')) || [];
-        clubs.push(newClub);
-        localStorage.setItem('clubs', JSON.stringify(clubs));
-
-        console.log("동아리가 등록되었습니다.");
-        this.navigateToSearch();
+data() {
+  return {
+    clubName: "",
+    leaderName: "",
+    leaderEmail: "", // leaderEmail로 변경
+    advisor: "",
+    maxMembers: "",
+    activitySchedule: "",
+    tags: "",
+    description: "",
+    isFormValid: false,
+  };
+},
+methods: {
+  async registerClub() {
+    if (this.isFormValid) {
+      const newClub = {
+        name: this.clubName,
+        leaderEmail: this.leaderEmail, // leaderEmail 추가
+        advisor: this.advisor,
+        maxMembers: this.maxMembers,
+        currentMembers: 0,
+        activitySchedule: this.activitySchedule,
+        tags: this.tags,
+        description: this.description
+      };
+      
+      try {
+        const response = await createClub(newClub);
+        if (response.success) {
+          this.$router.push({ name: "clubSearch" });
+        } else {
+          alert("동아리장 정보가 올바르지 않습니다.");
+        }
+      } catch (error) {
+        console.error("Error registering club", error);
       }
     }
   }
+}
 };
 </script>
 
-  
-  <style scoped>
-  .button-row {
-    margin-top: 20px;
-  }
-  </style>
+<style scoped>
+.button-row {
+margin-top: 20px;
+}
+</style>
   
   
